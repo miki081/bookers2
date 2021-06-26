3f99f2fe48cf7772d418163374bd86dbb581b4fa
miki081 <tgjmtp.kkmd125@gmail.com>
2021-06-26 11:43:42 +0000

    再修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,5 +20,12 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+         redirect_to user_path(@user)
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :image, :introduction)
   end
 end
